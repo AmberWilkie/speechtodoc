f84a14e5e03ccd67460b45e7eaed254c2b6fce48
Amber <amber@amberwilkie.com>
2017-02-13 18:04:27 +0100

    Add voice recognition js

---
+++ app/assets/javascripts/text_to_speech.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+    const recognition = new SpeechRecognition();
+    recognition.interimResults = true; // Allows program to work before you quit speaking.
+
+    var p = document.createElement('p');
+    const text_field = document.querySelector('.text_field');
+    text_field.appendChild(p);
+
+    recognition.addEventListener('result', e => {
+        const transcript = Array.from(e.results)
+                .map(result => result[0])
+    .map(result => result.transcript)
+    .join('');
+    p.textContent = transcript;
+    if (e.results[0].isFinal) {
+        p = document.createElement('p');
+        text_field.appendChild(p);
+    }
+});
+    recognition.addEventListener('end', recognition.start);
+    recognition.start();
+
+});
+
