d39f2d32532f1ac5199f190e6576cb30f2c1d5c1
Amber <amber@amberwilkie.com>
2017-02-13 21:40:34 +0100

    Get text to populate a text_area

--- app/assets/javascripts/text_to_speech.js
+++ app/assets/javascripts/text_to_speech.js
@@ -1,15 +1,17 @@
 $(document).ready(function() {
-    console.log(gon);
-
     window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
     const recognition = new SpeechRecognition();
     recognition.interimResults = true; // Allows program to work before you quit speaking.
 
     var p = document.createElement('p.document');
+    // var text;
     const text_field = document.querySelector('.text_field');
+
+    const text_area = document.querySelector('#doc_text_area');
+
     // const text_field_hidden = document.querySelector('.text_field_hidden');
-    text_field.appendChild(p);
+    // text_field.appendChild(p);
     // text_field_hidden.appendChild(p);
 
     recognition.addEventListener('result', e => {
@@ -17,21 +19,27 @@ $(document).ready(function() {
                 .map(result => result[0])
     .map(result => result.transcript)
     .join('');
-    p.textContent = transcript;
+    // p.textContent = transcript;
+    // text_area.value += transcript;
+    // text = transcript;
     if (e.results[0].isFinal) {
         p = document.createElement('p');
         // text_field_hidden.appendChild(p);
-        text_field.appendChild(p);
+        // text_field.appendChild(p);
+        text_area.value += "\n" + transcript;
+
+        // gon.global.document_text = text_field.innerHTML;
+        // console.dir(gon.global.document_text);
     }
 });
     recognition.addEventListener('end', recognition.start);
     recognition.start();
 
     function grabText() {
-        const text = text_field.innerHTML;
-        console.dir(text);
-        gon.document_text = text;
-        console.log(gon.document_text);
+        console.log(gon.global.document_text);
+        // const text = text_field.innerHTML;
+        // gon.document_text = text;
+        // console.log(gon.document_text);
     }
     document.querySelector('#document_save').addEventListener('click', grabText);
 
--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -3,10 +3,12 @@ class DocumentsController < ApplicationController
   def index
     # gon.clear
     # gon.document_text = 'default text'
-    @document_text = gon.document_text
+    # @document_text = gon.document_text
   end
 
   def create
+    binding.pry
+    @document_text = Gon.global.document_text
     render 'index'
   end
 end
